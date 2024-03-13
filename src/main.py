03eaeaf1f5e0e07a19254f6fe6615b2e836a73bc
Ian Matson <imatson9119@gmail.com>
2024-03-13 11:48:09 -0500

    Add headers

--- src/main.py
+++ src/main.py
@@ -61,63 +61,30 @@ def create_min_bible():
 	with open('bible-esv-min.json', 'w') as file:
 		json.dump(min_bible, file)
 
-def create_min_bible_2():
+def generate_headers_dictionary():
 	with open('bible-esv-formatted.json', 'r') as file:
-		bible: Bible = json.load(file)
+		bible: Bible = json.load(file)	
 	
-	min_bible = [
-		{
-			't': bible['metadata']['translation'],
-			'i': bible['metadata']['loc'],
-			'l': bible['metadata']['words'],
-			'nb': bible['metadata']['books'],
-			'nc': bible['metadata']['chapters'],
-			'nv': bible['metadata']['verses']
-		},
-		[]
-	]
+	metadata = {}
 
 	for book in bible['content']:
-		min_book = [
-			{
-				'b': book['metadata']['book'],
-				'i': book['metadata']['loc'],
-				'l': book['metadata']['words'],
-				'nc': book['metadata']['chapters'],
-				'nv': book['metadata']['verses']
-			},
-			[]
-		]
-
+		metadata[book['metadata']["loc"]] = {'b': book['metadata']['book']}
 		for chapter in book['content']:
-			min_chapter = [
-				{
-					'c': chapter['metadata']['chapter'],
-					'i': chapter['metadata']['loc'],
-					'l': chapter['metadata']['words'],
-					'nv': chapter['metadata']['verses']
-				},
-				[]
-			]
-
+			if chapter['metadata']['loc'] not in metadata:
+				metadata[chapter['metadata']['loc']] = {'c': chapter['metadata']['chapter']}
+			else:
+				metadata[chapter['metadata']['loc']]['c'] = chapter['metadata']['chapter']
 			for verse in chapter['content']:
-				min_verse = [
-					{
-						'v': verse['metadata']['verse'],
-						'i': verse['metadata']['loc'],
-						'l': verse['metadata']['words']
-					},
-					verse['content']
-				]
-
-				min_chapter[1].append(min_verse)
-
-			min_book[1].append(min_chapter)
-
-		min_bible[1].append(min_book)
+				if verse['metadata']['loc'] not in metadata:
+					metadata[verse['metadata']['loc']] = {'v': verse['metadata']['verse']}
+				else:
+					metadata[verse['metadata']['loc']]['v'] = verse['metadata']['verse']
 	
-	with open('bible-esv-min_2.json', 'w') as file:
-		json.dump(min_bible, file)
+	with open('bible-esv-headers.json', 'w') as file:
+		json.dump(metadata, file)
+	
+
+
 
 if __name__ == '__main__':
-	create_min_bible_2()
+	generate_headers_dictionary()
